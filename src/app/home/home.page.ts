18ece273df2a5a46804edc343dc78833b1cf04ea
5796 <5796@ait-sydney.net.au>
2018-11-13 16:50:51 +1100

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+  newfruit:string;
+  title:string = 'App Home Page';
+  fruits:Array<string> = ['mango','papayas','bananas'];
+
+  addFruit(){
+    if(this.newfruit.length>0){
+      this.fruits.push(this.newfruit);
+      this.newfruit="";
+    }
+
+  }
+}
