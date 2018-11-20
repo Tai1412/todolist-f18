598eb32fa127eb42e21e1e8d84df169c23753c8a
5796 <5796@ait-sydney.net.au>
2018-11-20 15:41:43 +1100

    added data service

---
+++ src/app/data.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {//share dta to other page
+
+  constructor() { }
+  //method to store data
+  storeList(list:Array<string>){//list:array<string> is argument
+    return new Promise((resolve,reject)=>{
+      let data =JSON.stringify( list );//local storage only accept stringify
+      try{
+        window.localStorage.setItem('list',data);//key and data in setItem, data gonna be list in storeList
+        if(window.localStorage.getItem('list')){
+            resolve(true);
+        }
+        else{
+          throw('save failed');
+        }
+      }
+      catch( error ){
+        reject( error );
+      }
+  });//restore when something done
+
+  }
+
+  //method to read data
+  loadList(){
+    return new Promise((resolve,reject)=>{
+      let data = window.localStorage.getItem('list');
+      if(data){
+        resolve(JSON.parse(data));//JSON parse convert string back to array because local storage only accept string
+      }
+      else{
+        reject(null);
+      }
+    })
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,33 @@ export class HomePage {
   newfruit:string;
   title:string = 'App Home Page';
   fruits:Array<string> = ['mango','papayas','bananas'];
+  constructor(public dataService:DataService){
+    this.readTask();//when hompage load its run this function
+  }
 
   addFruit(){
     if(this.newfruit.length>0){
       this.fruits.push(this.newfruit);
       this.newfruit="";
+      this.dataService.storeList(this.fruits)
+      .then((response)=>{
+        //successs
+      })
+      .catch((error)=>{
+        console.log(error);//javascript here
+      });
     }
 
+  }
+  readTask(){//when refresh page read the task
+    this.dataService.loadList()
+    .then((response)=>{
+      if(response!==null){
+        this.fruits=<Array<string>>response;
+      }
+    })
+    .catch((error)=>{
+      console.log(error);
+    });
   }
 }
