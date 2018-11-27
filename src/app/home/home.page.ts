e65335d391acaf95fd24298e5784540ae881ea2a
5796 <5796@ait-sydney.net.au>
2018-11-27 16:56:30 +1100

    add done function, checkbox, set time, sort item, and delete item

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,8 +11,10 @@ export class HomePage {
   taskInput:string;
   title:string = 'App Home Page';
   tasks:Array<Task> = [];
+  now:number;
   constructor(public dataService:DataService){
     this.readTask();//when hompage load its run this function
+    this.now = new Date().getTime();
   }
   createTask(taskName:string){
     let taskDate:number=new Date().getTime();
@@ -31,6 +33,7 @@ export class HomePage {
       .catch((error)=>{
         console.log(error);//javascript here
       });
+      this.sortItems();
     }
 
   }
@@ -44,5 +47,50 @@ export class HomePage {
     .catch((error)=>{
       console.log(error);
     });
+  }
+
+  //change a task's status
+  changeStatus(date){
+    this.tasks.forEach((task)=>{
+      if(task.date==date){
+        task.status= task.status ? false : true;
+      }
+
+    });
+    this.dataService.storeList(this.tasks);//the count on status
+    this.sortItems();
+  }
+
+  deleteItem(date){
+    this.tasks.forEach((task,index)=>{
+      if(task.date==date){
+        this.tasks.splice(index,1);//use splice method to remove item
+      }
+    });
+      this.sortItems();
+      this.dataService.storeList(this.tasks);
+  }
+  formatDate(date:number){
+    let diff=this.now -date;
+    let seconds=diff/1000;
+    if( seconds <=60){
+      return 'just now';
+    }
+    else if(seconds >= 60 && seconds <= 3600)
+    {
+      let mins=Math.floor(seconds/60);
+      let mUnit=mins ==1? 'minute' : 'minutes';
+      return mins+ ' '+mUnit+'ago';
+    }
+  }
+
+  sortItems(){
+    //sort by date
+    this.tasks.sort((task1,task2)=>{
+      if(task1.date<task2.date){return 1}
+      if(task1.date>task2.date){return -1}
+      if(task1.date == task2.date){return 0}
+    });
+    this.dataService.storeList(this.tasks);
   }
 }
